5bec8b46b21d5619fed10f46d8f804d6a307e676
BeagleXV <146370309+BeagleXV@users.noreply.github.com>
2024-08-09 21:40:56 -0300

    Update atividade01.py

--- DesafiosRodrigoMoreira/atividade01.py
+++ DesafiosRodrigoMoreira/atividade01.py
@@ -1,6 +1,6 @@
 #Escreva um programa que imprima seu nome e sua cidade natal.
 
-nome = "Fenando"
+nome = "Fernando"
 cidade_natal = "Rio de Janeiro"
 
 print(f"Nome: {nome}\nCidade natal: {cidade_natal}")
