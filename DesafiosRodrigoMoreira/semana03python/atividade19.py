4ccf25e1838603acf258fde722ad2c1ba7daeeef
BeagleXV <beaglexv@gmail.com>
2024-08-21 13:10:31 -0300

    atualizacao atividade19.py

--- DesafiosRodrigoMoreira/semana03python/atividade19.py
+++ DesafiosRodrigoMoreira/semana03python/atividade19.py
@@ -6,5 +6,5 @@ for nota in range(1,4):
     valor = float(input(f"Qual valor da {nota}ª nota: "))
     notas.append(valor)
 media_ponderada = (notas[0] * 2 + notas[1] * 3 + notas[2] * 5) / 10
-print(f"A média ponderada das {notas[0]}, {notas[1]} e {notas[2]} é: {media_ponderada}")
+print(f"A média ponderada das {notas[0]}, {notas[1]} e {notas[2]} é: {media_ponderada:.2f}")
 
